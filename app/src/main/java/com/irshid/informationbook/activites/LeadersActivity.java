5d37e767af39eafd15d708bd9eb92cdd485d69b1
Ahmad Irshid <ahmadwirshid@gmail.com>
2023-03-20 19:17:39 -0700

    first commit

---
+++ app/src/main/java/com/irshid/informationbook/activites/LeadersActivity.java
@@ -0,0 +1,16 @@
+package com.irshid.informationbook.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.irshid.informationbook.R;
+
+public class LeadersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaders);
+    }
+}
